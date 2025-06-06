dbe4a62a5255ae5d602ae186763f9c1a92c600b2
shine85 <hanhui7413@gmail.com>
2025-06-06 14:37:01 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://81.69.248.200:21114".to_owned()
 }
 
 #[inline]
